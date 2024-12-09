2f18bacad66a40f14836741e634d122e4fdcfbac
stphgl13 <glassstephanie@outlook.com>
2024-12-09 02:19:09 -0500

    progress
    pushing

---
+++ script.js
@@ -0,0 +1,46 @@
+
+    
+        function nextbutt() {
+            const currentArticle = document.getElementById(allArticles[currentIndex]);
+            
+        
+        
+            }
+    
+    
+       
+        
+document.getElementById('buttnext').addEventListener('click', nextbutt);
+document.getElementById('hidebutt').addEventListener('click', hideit);
+
+
+
+document.getElementById("hefont1").style.fontSize = "50pt";
+document.getElementById("hefont2").style.fontSize = "50pt";
+document.getElementById("hefont3").style.fontSize = "50pt";
+
+document.getElementById("pfont1").style.fontSize = "25pt";
+document.getElementById("pfont2").style.fontSize = "25pt";
+document.getElementById("pfont3").style.fontSize = "25pt";
+
+
+document.getElementById("rating1").innerHTML = "⭐⭐⭐⭐⭐"
+document.getElementById("rating2").innerHTML = "⭐⭐⭐⭐"
+document.getElementById("rating3").innerHTML = "⭐⭐"
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
