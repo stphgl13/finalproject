7db177e85f3d88434cd9d717144bbdc3c015b80a
stphgl13 <glassstephanie@outlook.com>
2024-12-12 11:38:42 -0500

    tons of changes
    basically everything currently here looks way better and is decently functional. Carosel reyturned to the article showcase. possibly going to revamp cow page
    ???

--- script.js
+++ script.js
@@ -3,20 +3,14 @@
 
 
 
-document.getElementById("hefont1").style.fontSize = "50pt";
-document.getElementById("hefont2").style.fontSize = "50pt";
-document.getElementById("hefont3").style.fontSize = "50pt";
-
-document.getElementById("pfont1").style.fontSize = "25pt";
-document.getElementById("pfont2").style.fontSize = "25pt";
-document.getElementById("pfont3").style.fontSize = "25pt";
 
 
-document.getElementById("rating1").innerHTML = "⭐⭐⭐⭐⭐"
-document.getElementById("rating2").innerHTML = "⭐⭐⭐⭐"
-document.getElementById("rating3").innerHTML = "⭐⭐"
-
-
+document.querySelector('.ccn').addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' 
+    });
+  });
 
 
 
